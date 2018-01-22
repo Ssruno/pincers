b9c722de22df3730c063de381f4e9768af6dfd0d
Bruno Duarte <100697+Ssruno@users.noreply.github.com>
2018-01-22 03:23:42 -0300

    Proposal of implementation for closest() tree traversal utility platanus/pincers#18

--- lib/pincers/core/search_context.rb
+++ lib/pincers/core/search_context.rb
@@ -93,6 +93,19 @@ module Pincers::Core
       if elements.last.nil? then nil else wrap_siblings [elements.last] end
     end
 
+    # Implementation of closest() from jQuery
+    def closest(_selector=nil)
+      if _selector
+        parser = Pincers::CSS::Parser.new _selector
+        exp = parser.to_xpath("ancestor-or-self::")
+        exp = exp.first if exp.length == 1
+
+        search(:xpath => "#{exp}[1]")
+      else
+        raise ArgumentError, 'Selector was expected.'
+      end
+    end
+
     def search(_selector=nil, _options={}, &_block)
       if _selector.is_a? Hash
         _options = _selector
